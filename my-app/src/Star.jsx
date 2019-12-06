b721549f8bf3154f2fc982befa47d2f5232021ef
karl74 <carlos.castro74@gmail.com>
2019-12-06 14:41:02 -0600

    changes

--- my-app/src/Star.jsx
+++ my-app/src/Star.jsx
@@ -2,8 +2,7 @@ import React from "react"
 
 const Star= ({selected= false, onClick= f=>f})=>
 	<div className={(selected) ? "star selected" : "star"}
-	onClick={onClick}
-	>
+	onClick={onClick}>
 	</div>
 
 	
